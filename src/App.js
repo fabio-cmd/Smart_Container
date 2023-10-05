0197442128bc81cd76b7fd53c206c3bf12845078
Fábio <fdiniz704@gmail.com>
2023-10-04 21:27:55 -0300

    fix remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 import TextSection from './components/TextSection/TextSection';
 import SignupSection from './components/SignupSection/SignupSection';
 import Header from './components/Header/Header';
@@ -10,11 +8,9 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-   
       <Header/>
       <TextSection/>
       <SignupSection/>
-   
     </div>
   );  
 }
